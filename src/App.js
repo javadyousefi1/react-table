2e89cf9b8c19dfcf215e68d0613adb4fe5b5c874
javadyousefi <javadyousefi9000@gmail.com>
2023-10-07 17:35:18 +0330

    Test username

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Table from "./components/table";
 const App = () => {
   return (
     <>
-      <div className="h-screen bg-[#f6f7f9]">
-        <h1 className="bg-blue-500 text-white text-center p-2 text-lg font-bold">
+      <div dir="rtl" className="h-screen ">
+        <h1 className="bg-blue-500 text-white text-center p-2 text-lg font-bold mb-20">
           TIC React Table
         </h1>
 
