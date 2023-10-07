f555c3201266ceb1c2fd5ade4f446111443edbe4
erfaneh <mailto:erfaneh.s7713@gmail.com>
2023-10-07 16:59:04 +0330

    table component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Table from "./components/table";
+
 const App = () => {
-  return ( 
+  return (
     <>
-    test
+      <div className="h-screen bg-[#f6f7f9]">
+        <h1 className="bg-blue-500 text-white text-center p-2 text-lg font-bold">
+          TIC React Table
+        </h1>
+
+        <Table />
+      </div>
     </>
-   );
-}
- 
+  );
+};
+
 export default App;
---
+++ src/components/table.jsx
@@ -0,0 +1,9 @@
+const Table = () => {
+  return (
+    <div className="container mx-auto max-w-6xl">
+      <table></table>
+    </div>
+  );
+};
+
+export default Table;
